bbaacc2aca4fe46f38c12635bd7c26ff2da520d7
Mohamed Zaheer <137611200+Zaheer2003@users.noreply.github.com>
2024-07-09 00:50:46 +0530

    Add files via upload

---
+++ Contacts.php
@@ -0,0 +1,18 @@
+<?php include 'header.php'; ?>
+
+<section id="contact">
+    <p class="section__text__p1">Get in Touch</p>
+    <h1 class="title">Contact Me</h1>
+    <div class="contact-info-upper-container">
+        <div class="contact-info-container">
+            <img src="./assets/email.png" alt="Email icon" class="icon contact-icon email-icon">
+            <p><a href="mailto:mhdzaheer2003@gmail.com">mhdzaheer2003@gmail.com</a></p>
+        </div>
+        <div class="contact-info-container">
+            <img src="./assets/linkedin.png" alt="LinkedIn icon" class="icon contact-icon">
+            <p><a href="https://www.linkedin.com/in/mohamed-zaheer-335292246?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">LinkedIn</a></p>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php'; ?>
